b70f40f5fc0132d5fda096d1e789dcfe3d60f974
Emine Shaban <emineshaban@icloud.com>
2023-05-14 19:27:42 +0300

    .

--- game-countries/src/App.js
+++ game-countries/src/App.js
@@ -7,63 +7,46 @@ import CountryList from './components/CountryList';
 
 
 function App() {
-  const [countriesDetails, setCountriesDetails] = useState([]);
-  // const [country, setCountry] = useState([])
-  
-  //   useEffect(() => {
-  //     countryService.getAll()
-  //     .then(setCountry)
-  //     .catch(err =>{
-        
-  //       console.log(err)
-  //     })
-  //   },[])
-  // console.log(countriesDetails)
 
-console.log('1')
-// useEffect(() => {});
+  const [countriesDetails, setCountriesDetails] = useState();
 
-useEffect(() => {
-  console.log('1.5')
+  useEffect(() => {
 
-  async function getCountryDetails() {
-    const url = "https://restcountries.com/v3.1/all";
-    try {
-      console.log('1.5')
+    async function getCountryDetails() {
+      const url = "https://restcountries.com/v3.1/all";
+      try {
+        console.log('1.5')
 
-      const response = await (await fetch(url)).json();
-      console.log('1.5')
+        const response = await (await fetch(url)).json();
+        console.log('1.5')
 
-      const details = response?.map((country) => {
-        const countryDetail = {
-          name: country.name.common,
-          capital: country.capital,
-          flags: country.flags.png,
-        };
-        // console.log(countryDetail)
-        return countryDetail;
-      });
-// console.log(details)
-      setCountriesDetails(details);
-    } catch (error) {
-      // console.log('1.5')
-
-      console.log(error);
+        const details = response.map((country) => {
+          const countryDetail = {
+            name: country.name.common,
+            capital: country.capital,
+            flags: country.flags.png,
+          };
+          return countryDetail;
+        });
+        console.log(details)
+        setCountriesDetails(details);
+      } catch (error) {
+        console.log(error);
+      }
     }
-  }
-  getCountryDetails();
-}, []);
+    getCountryDetails();
+  }, []);
 
-console.log('2')
+  console.log('2')
 
 
   return (
     <div>
 
       <p>Hello, </p>
-    <CountryList countriesDetails={countriesDetails}/>
+      <CountryList countriesDetails={countriesDetails} />
     </div>
-    
+
   );
 }
 
--- game-countries/src/components/CountryList.js
+++ game-countries/src/components/CountryList.js
@@ -1,48 +1,97 @@
 // import Country from "./Country"
-import { useState, useEffect } from "react"
+
+import shuffle from "../services/shuffle"
+
+// import { useState, useEffect } from "react"
 export default function CountryList({
     countriesDetails,
 
 }) {
-    console.log('jj')
-    console.log(countriesDetails)
-    const [country, setCountry] = useState([])
 
-    let a = Number(Math.round(Math.random() * 10))
-    useEffect(() => {
+    let result = []
+    let a = 0
+    // console.log(result)
+    let rightAnswer = null
+    // const [country, setCountry] = useState();
 
-        // console.log(a)
-        // console.log(countriesDetails[a])
-        const first = countriesDetails[a]
-        // console.log(first)
-                        .then(setCountry(first))
-                .catch(err =>{
-                  console.log(err)
-                })
-              },[])
+    // useEffect(() => {
+    function randomCountry() {
+        if (countriesDetails) {
+            a = Number(Math.round(Math.random() * countriesDetails.length))
+            result = countriesDetails[a]
+            rightAnswer = result.name
+            console.log(result)
+            console.log(rightAnswer)
+            // setCountry(result)
+
+        }
+        console.log(result)
+
+    }
+    randomCountry()
+
+    let optionAnswersIndex = []
+    let optionAnswers = []
+    function randomAnswers() {
+
+        if (countriesDetails) {
+            // console.log(rundomIndex)
+            for (let index = 0; index < 3; index++) {
+                let rundomIndex = Number(Math.round(Math.random() * countriesDetails.length))
+                if (rundomIndex !== a) {
+                    optionAnswersIndex.push(rundomIndex)
+                    // optionAnswers.push(countriesDetails[rundomIndex])
+                } else {
+                    index -= 1
+                }
+
+            }
+            optionAnswersIndex.push(a)
+
+            console.log(optionAnswersIndex)
+            console.log(a)
+            for (let index = 0; index < optionAnswersIndex.length; index++) {
+                // let r = Number(Math.round(Math.random() * optionAnswersIndex.length))
+                // console.log(r)
+                optionAnswers.push(countriesDetails[optionAnswersIndex[index]])
+            }
+  
+
+shuffle(optionAnswers)
+
+
+            
+        }
+    }
     
+    randomAnswers()
+    
+  
 
 
 
-   
+
+
     return (
         <div>
             <p>
-                 {country?.capital} is the capital of?
+                Hello!
+                {result.capital} is the capital of?
             </p>
 
+            
+                {optionAnswers.map((countries, index) =>(
 
+                    <div key={index}>
+                        <p>
 
-
-
-            {/* {countriesDetails.map((c, index) => 
-      // console.log(index)
-      <Country key={index} {...c}/>
-      )
-      } */}
+                        {countries.name}
+                        </p>
+                    </div>
+                ))}
+ 
 
         </div>
     );
-    // })}
 
 }
